3c2f27ecc4ab4bacd78ae66f32d24d8b121a23a1
ishu kumar <ishukumar69.49@gmail.com>
2025-02-14 21:22:09 +0530

    created ui and task add feature

---
+++ script.js
@@ -0,0 +1,45 @@
+const addBtn = document.querySelector('.add-btn');
+const input = document.querySelector("#task-input");
+const tasksContainer = document.querySelector(".tasks-continer")
+
+addBtn.addEventListener("click", () =>{
+     let task = input.value;
+     if(task.length>0) {
+         const checkbox = document.createElement("input");
+         checkbox.setAttribute("type", "checkbox");
+         checkbox.classList.add("check");
+
+
+         const taskName = document.createElement("span");
+         taskName.innerText = task;
+         taskName.classList.add("task");
+
+         const deletBtn = document.createElement("button");
+         deletBtn.innerText = "delet";
+         deletBtn.classList.add("delete-btn");
+
+         const tasks = document.createElement("li");
+         tasks.classList.add("task-continer")
+         tasks.appendChild(checkbox);
+         tasks.appendChild(taskName);
+         tasks.appendChild(deletBtn);
+
+         console.log(tasks)
+
+
+         const taskContainer = document.querySelector(".tasks-continer");
+         taskContainer.appendChild(tasks);
+
+         console.log(checkbox);
+         console.log(taskName);
+         console.log(deletBtn);
+
+     }
+    
+    
+    input.value = "";
+})
+
+tasksContainer.addEventListener("click", function(el) {
+    console.log(el);
+})
